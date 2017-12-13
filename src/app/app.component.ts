396f1afb28a8ae519228c27260faddade321e146
Jordsan <mail.jordan.michaels@gmail.com>
2017-12-13 16:20:07 -0500

    basic tab highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    currTab = 0;
 
+    setCurrTab(input: number): void {
+        this.currTab = input;
+    }
+
+    getHighlightClass(input: number): string {
+        if (input === this.currTab) {
+            return 'selected';
+        }
+        else {
+            return 'not-selected';
+        }
+    }
 }
