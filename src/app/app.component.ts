679253f33eb58fb9686fc5c815747ae6e8f23719
Jordsan <mail.jordan.michaels@gmail.com>
2017-12-15 02:30:37 -0500

    tab deep linking fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    currTab = 0;
+
+    currTab;
+
+    constructor(private router: Router) {
+
+    }
 
     setCurrTab(input: number): void {
         this.currTab = input;
     }
 
     getHighlightClass(input: number): string {
+        switch (input) {
+            case 0: {
+                if (this.router.url.includes('home')) {
+                    return 'selected';
+                }
+                else {
+                    return 'not-selected';
+                }
+            }
+            case 1: {
+                if (this.router.url.includes('about')) {
+                    return 'selected';
+                }
+                else {
+                    return 'not-selected';
+                }
+            }
+            case 2: {
+                if (this.router.url.includes('projects')) {
+                    return 'selected';
+                }
+                else {
+                    return 'not-selected';
+                }
+            }
+        }
         if (input === this.currTab) {
             return 'selected';
         }
