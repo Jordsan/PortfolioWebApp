edd47eda258920b6e22e837e6973f096b3610685
Jordsan <mail.jordan.michaels@gmail.com>
2017-12-24 15:55:09 -0500

    updated animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,22 +12,40 @@ import {
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
     animations: [
-        trigger('slideIn', [
-            state('in', style({ transform: 'translateX(0)' })),
+        // trigger('slideIn', [
+        //     state('in', style({ transform: 'translateX(0)' })),
+        //     transition('void => *', [
+        //         style({ transform: 'translateX(-100%)' }),
+        //         animate(250)
+        //     ]),
+        //     transition('* => void', [
+        //         animate(250, style({ transform: 'translateX(-100%)' }))
+        //     ])
+        // ]),
+        trigger('fadeIn', [
+            state('in', style({ opacity: 1 })),
             transition('void => *', [
-                style({ transform: 'translateX(-100%)' }),
-                animate(250)
+                style({ opacity: 0 }),
+                animate('150ms 150ms')
             ]),
             transition('* => void', [
-                animate(250, style({ transform: 'translateX(-100%)' }))
+                animate(100, style({ opacity: 0 }))
             ])
+            // state('icon-hovered', style({
+            //     opacity: 1
+            // })),
+            // state('icon-not-hovered', style({
+            //     opacity: 0
+            // })),
+            // transition('icon-not-hovered => icon-hovered', animate('250ms ease-in')),
+            // transition('icon-hovered => icon-not-hovered', animate('250ms ease-out'))
         ]),
         trigger('hoverIcon', [
             state('icon-hovered', style({
-                transform: 'scale(1.3)'
+                transform: 'scale(3.6)', opacity: 0.05
             })),
             state('icon-not-hovered', style({
-                transform: 'scale(1)'
+                transform: 'scale(1)', opacity: 1
             })),
             transition('icon-not-hovered => icon-hovered', animate('250ms ease-in')),
             transition('icon-hovered => icon-not-hovered', animate('250ms ease-out'))
@@ -39,6 +57,7 @@ export class HomeComponent implements OnInit {
     hoverHomeVal: boolean;
     hoverPhoneVal: boolean;
     hoverEmailVal: boolean;
+    hoverResumeVal: boolean;
     hoverLinkedInVal: boolean;
     hoverGithubVal: boolean;
 
@@ -50,6 +69,7 @@ export class HomeComponent implements OnInit {
         this.hoverHomeVal = false;
         this.hoverPhoneVal = false;
         this.hoverEmailVal = false;
+        this.hoverResumeVal = false;
         this.hoverLinkedInVal = false;
         this.hoverGithubVal = false;
     }
@@ -68,6 +88,10 @@ export class HomeComponent implements OnInit {
                 const val = this.hoverEmailVal ? 'icon-hovered' : 'icon-not-hovered';
                 return val;
             }
+            case 'resume': {
+                const val = this.hoverResumeVal ? 'icon-hovered' : 'icon-not-hovered';
+                return val;
+            }
             case 'linkedin': {
                 const val = this.hoverLinkedInVal ? 'icon-hovered' : 'icon-not-hovered';
                 return val;
@@ -77,10 +101,6 @@ export class HomeComponent implements OnInit {
                 return val;
             }
         }
-    }
-
-    hover(input: string): void {
-
     }
 
     hoverHome(): void {
@@ -93,6 +113,10 @@ export class HomeComponent implements OnInit {
 
     hoverEmail(): void {
         this.hoverEmailVal = !this.hoverEmailVal;
+    }
+
+    hoverResume(): void {
+        this.hoverResumeVal = !this.hoverResumeVal;
     }
 
     hoverLinkedIn(): void {
